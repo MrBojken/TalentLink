cc880dc093e68267a75a182f591a33aa7bc6a091
MrBojken <57535147+Avoking@users.noreply.github.com>
2025-09-03 21:37:50 +0200

    Prepare project for Render deployment

--- jobboard/settings.py
+++ jobboard/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-sa-1264^hpmx#2bahoqo+cz@owksad9a#t%g41+=9f+nd%aykp'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
